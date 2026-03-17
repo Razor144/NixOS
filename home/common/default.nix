cff8a8f3c18be5a62632041a55140d64f46ed6dc
Chris <144razor@googlemail.com>
2026-03-17 12:30:04 +0100

    add NUM

--- home/common/default.nix
+++ home/common/default.nix
@@ -11,8 +11,19 @@
 
   home.packages = with pkgs; [
     bitwarden-desktop
+    numlockx
     spotify
   ];
+
+  xdg.configFile."autostart/numlockx.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Enable Numlock
+    Exec=${pkgs.numlockx}/bin/numlockx on
+    OnlyShowIn=KDE;
+    X-KDE-Autostart-phase=1
+    NoDisplay=true
+  '';
 
   programs.git.enable = true;
 }
